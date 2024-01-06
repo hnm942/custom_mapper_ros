ea05e3d8a623cc27ab3155c29cfafe0609bab9c6
hnm942 <huy.nm170773@gmail.com>
2024-01-06 22:27:30 +0700

    start association phase

---
+++ custom_mapper_ros/scripts/get_depth.py
@@ -0,0 +1,21 @@
+import open3d as o3d
+import rospy
+import rosbag 
+import numpy as np 
+import matplotlib.pyplot as plt
+from cv_bridge import CvBridge
+
+if __name__ == "__main__":
+    bag = rosbag.Bag("/home/hnm942/workspace/src/custom_mapper_ros/custom_mapper_ros/data/rgbd_dataset_freiburg1_360.bag")
+    camera_msg = None
+    bridge = CvBridge()
+
+
+    for topic, msg, t in bag.read_messages(topics = ["/camera/depth/image"]):
+        img_msg = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+
+        np.save("/home/hnm942/workspace/src/custom_mapper_ros/custom_mapper_ros/data/rgbd_dataset_freiburg1_360/color_images/image_depth_{}.npy".format(t), img_msg)
+    print("a")
+    bag.close()
+
+    
